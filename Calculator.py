54dec5fb9c25c6f048e6fd474a42b4631c28ff64
jagx007 <120235811+jagx007@users.noreply.github.com>
2022-12-10 20:17:58 +0530

    Calculator

---
+++ Calculator.py
@@ -0,0 +1,62 @@
+print('\t \t CALCULATOR')
+
+
+def add():
+    x = a + b
+    return x
+
+
+def sub():
+    x = a - b
+    return x
+
+
+def mul():
+    x = a * b
+    return x
+
+
+def div():
+    x = a / b
+    return x
+
+def mod():
+    x = a % b
+    return x
+
+def pow():
+    x = a ** b
+    return x
+
+
+while True:
+    a = int(input('Enter a number: '))
+    b = int(input('Enter a number: '))
+    c = int(input('\t1. ADD \n \t2. SUBSTRACT \n \t3. MULTIPLY \n \t4. DIVIDE \n \t5. MODULUS \n \t6. POWER \n \t enter: '))
+
+    if c == 1:
+        print(f'{a} + {b} = ', add())
+
+    elif c == 2:
+        print(f'{a} - {b} = ', sub())
+
+    elif c == 3:
+        print(f'{a} x {b} = ', mul())
+
+    elif c == 4:
+        print(f'{a} / {b} = ', div())
+
+    elif c == 5:
+        print(f'{a} % {b} = ', mod())
+
+    elif c == 6:
+        print(f'{a} ^ {b} = ', pow())
+
+    else:
+        print('Syntax error')
+
+    q = input('Do you want to continue(y/n): ')
+    if q in 'Nn':
+        break
+    else:
+        continue
